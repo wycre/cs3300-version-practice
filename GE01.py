6045e7917f83270e1fd87a5c8b5853853ce6452c
Aclamendo <davidjones@ycre.net>
2024-01-25 11:52:03 -0700

    complete tutorial

---
+++ GE01.py
@@ -0,0 +1,104 @@
+## Lists
+numbers = []
+strings = []
+names = ["John", "Eric", "Jessica"]
+
+# write your code here
+second_name = names[1]
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+strings.append("hello")
+strings.append("world")
+
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
+
+
+## Basic Operators
+x = object()
+y = object()
+
+# TODO: change this code
+x_list = [x] * 10
+y_list = [y] * 10
+big_list = x_list + y_list
+
+print("x_list contains %d objects" % len(x_list))
+print("y_list contains %d objects" % len(y_list))
+print("big_list contains %d objects" % len(big_list))
+
+# testing code
+if x_list.count(x) == 10 and y_list.count(y) == 10:
+    print("Almost there...")
+if big_list.count(x) == 10 and big_list.count(y) == 10:
+    print("Great!")
+
+
+## Functions
+# Modify this function to return a list of strings as defined above
+def list_benefits():
+    return ["More organized code", "More readable code", "Easier code reuse",
+             "Allowing programmers to share and connect code together"]
+
+# Modify this function to concatenate to each benefit - " is a benefit of functions!"
+def build_sentence(benefit):
+    return " is a benefit of functions!"
+
+def name_the_benefits_of_functions():
+    list_of_benefits = list_benefits()
+    for benefit in list_of_benefits:
+        print(build_sentence(benefit))
+
+name_the_benefits_of_functions()
+
+
+## Classes & Objects
+# define the Vehicle class
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str
+# your code goes here
+car1 = Vehicle()
+car1.name = "Fer"
+car1.kind = "convertible"
+car1.color = "red"
+car1.value = 60000
+
+car2 = Vehicle()
+car2.name = "Jump"
+car2.kind = "van"
+car2.color = "blue"
+car2.value = 10000
+
+# test code
+print(car1.description())
+print(car2.description())
+
+
+
+## Dictionaries
+phonebook = {  
+    "John" : 938477566,
+    "Jack" : 938377264,
+    "Jill" : 947662781
+}  
+# your code goes here
+phonebook["Jake"] = 938273443
+
+phonebook.pop("Jill")
+
+# testing code
+if "Jake" in phonebook:  
+    print("Jake is listed in the phonebook.")
+    
+if "Jill" not in phonebook:      
+    print("Jill is not listed in the phonebook.")  
